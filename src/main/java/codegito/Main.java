7d1f57ce271b12423e0f80f6516b36ac0cdd4ae0
DevLuiggi <luiggipdeveloper@gmail.com>
2025-09-06 23:18:42 -0500

    feat(myFirstProject): My first project in Java in Hyperskill

--- src/main/java/codegito/Main.java
+++ src/main/java/codegito/Main.java
@@ -2,9 +2,11 @@ package codegito;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         List<Product> products = new ArrayList<>();
         products.add(new Product("Bubblegum", 2, 202));
         products.add(new Product("Toffee", 0.2, 118));
@@ -24,6 +26,24 @@ public class Main {
             }
             total += earnings;
         }
-        System.out.println("\nIncome: " + "$" + total);
+        if (total == (int) total){
+            System.out.println("\nIncome: " + "$" + (int) total);
+        }
+        else {
+            System.out.println("\nIncome: " + "$" + total);
+        }
+        System.out.println("Staff expenses:");
+        double totalExp = input.nextDouble();
+        total -= totalExp;
+        System.out.println("Other expenses: ");
+        double totalOtherExp = input.nextDouble();
+        total -= totalOtherExp;
+        if (total == (int) total){
+            System.out.println("Net income: $" + (int) total);
+        }
+        else {
+            System.out.println("Net income: $" + total);
+        }
+        input.close();
     }
 }
